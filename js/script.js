1c33186ae4cceb8dbd6f7b4763c1ac31a133c4a4
annanwang <annanwang@sandiego.edu>
2017-10-08 17:05:29 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 $(init);
 
-function init()
-{
-    $("#resizeMe").resizable();
+function init() {
+  $("#resizeMe").resizable();
 
-    $(".mainImage").addClass("ui-widget")
-            .addClass("ui-widget-content")
-            .addClass("ui-corner-all");
+  $(".mainImage").addClass("ui-widget")
+    .addClass("ui-widget-content")
+    .addClass("ui-corner-all");
+
+  $(".dragMe").draggable();
+  $("#target").droppable();
+
+  $("#target").bind("drop", highlightTarget);
+  $("#target").bind("dropout", resetTarget);
+
+  function highlightTarget(event, ui) {
+    $("#target").addClass("ui-state-highlight")
+      .html("Dropped ")
+      .append(ui.draggable.text());
+  }
+
+  function resetTarget(event, ui) {
+    $("#target").removeClass("ui-state-highlight")
+      .html("Drop on me");
+  }
 }
